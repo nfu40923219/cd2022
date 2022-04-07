4f9cd272e6a930596beb50e7bdc8f2092da35907
nfu40923219 <llleo0302@gmail.com>
2022-04-07 14:32:51 +0800

    add text

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'cmsimde_site', 'text': '40923219 \n 測試測試測', 'tags': '', 'url': 'cmsimde_site.html'}]};
+var tipuesearch = {"pages": [{'title': 'cmsimde_site', 'text': '40923219 \n [在此插入繪製零件的過程]', 'tags': '', 'url': 'cmsimde_site.html'}]};
